e0d5fef9271ae93478eef1425f37e075db4a6fb5
hosseinnejatijavaremi <hosseinnejati14@gmail.com>
2018-12-30 14:25:01 +0330

    convert sql query 17 to spark sql api

--- spark_query/query_in_parquet_format/spark_query17.py
+++ spark_query/query_in_parquet_format/spark_query17.py
@@ -16,6 +16,17 @@ region = sqlContext.read.parquet("hdfs://namenode:8020/hossein-parquet-data/regi
 
 from pyspark.sql import functions as F
 
-fun1 = lambda x, y: x * (1 - y)
+fun1 = lambda x: x * 0.2
 
+part_lineitem_filter = part.filter((part.P_BRAND == "Brand#23") & (part.P_CONTAINER == "JUMBO JAR")) \
+    .select(part.P_PARTKEY) \
+    .join(lineitem, part.P_PARTKEY == lineitem.L_PARTKEY, "left_outer")
 
+query17 = part_lineitem_filter.groupBy(part_lineitem_filter.P_PARTKEY) \
+    .agg(fun1(F.avg(part_lineitem_filter.L_QUANTITY)).alias("avg_quantity"))
+
+query17 = query17.select(query17.P_PARTKEY.alias("key"), query17.avg_quantity)
+
+query17 = query17.join(part_lineitem_filter, query17.key == part_lineitem_filter.P_PARTKEY) \
+    .filter(part_lineitem_filter.L_QUANTITY < query17.avg_quantity) \
+    .agg(F.sum(part_lineitem_filter.L_EXTENDEDPRICE) / 7.0)
